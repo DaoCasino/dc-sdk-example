9c0aa306d741659c1891c0b78593798c8927e24a
Your Name <you@example.com>
2018-10-25 11:03:55 +0300

    add without connection to dc-sdk-example

--- dapp/dapp.manifest.js
+++ dapp/dapp.manifest.js
@@ -4,12 +4,12 @@ export default {
   // if you want to change filename  - change it too in /scripts/config/paths
   logic: './dapp.logic.js',
   ERC20: {
-    address: require('../../dc-protocol/v_0.1/build/addresses.json').ERC20,
-    abi: require('../../dc-protocol/v_0.1/build/contracts/ERC20.json')
+    address: require('../../dc-protocol/src/build/addresses.json').ERC20,
+    abi: require('../../dc-protocol/src/build/contracts/ERC20.json')
   },
   contract: {
-    address: require('../../dc-protocol/v_0.1/build/addresses.json').Game,
-    abi: require('../../dc-protocol/v_0.1/build/contracts/myDAppGame.json').abi
+    address: require('../../dc-protocol/src/build/addresses.json').Game,
+    abi: require('../../dc-protocol/src/build/contracts/myDAppGame.json').abi
   },
   rules: {
     depositX: 2
