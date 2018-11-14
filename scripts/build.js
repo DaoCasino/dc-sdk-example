3d52b9286f4be6b8178710347b6316d52d721226
Alexey Litivnov <alex.o.litvinov@gmail.com>
2018-11-14 12:50:42 +0300

    dc-sdk-example fix env.CI

--- scripts/build.js
+++ scripts/build.js
@@ -117,8 +117,7 @@ function build (previousFileSizes) {
       if (messages.errors.length) {
         return reject(new Error(messages.errors.join('\n\n')))
       }
-      process.env.CI = false
-      if (process.env.CI && messages.warnings.length) {
+      if (process.env.CI == true && messages.warnings.length) {
         console.log(chalk.yellow(
           '\nTreating warnings as errors because process.env.CI = true.\n' +
           'Most CI servers set it automatically.\n'
