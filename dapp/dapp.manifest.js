c047ce844589d6d85c3a1ea999660c8cccaad883
Your Name <you@example.com>
2018-11-19 16:39:58 +0300

    cross-env add

--- dapp/dapp.manifest.js
+++ dapp/dapp.manifest.js
@@ -1,6 +1,6 @@
 const addressFunctions = {
   rinkeby: () => "0x868944cd75d4b70b6fb59254e998d5f757d7de0c",
-  ropsten: () => "0xf4dac7a329bcabc02c62d438d1f2dd226680b6f6",
+  ropsten: () => "0x2d52802d5339EA8FBbDC21BA2ED651744dF8a6eA",
   mainnet: () => "",
   local: () => "http://localhost:8545/contracts->Game"
 }
