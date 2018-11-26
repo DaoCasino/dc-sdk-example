513ac68af6e93e5f93e3fd211525929cf900859e
Michael Oznobkin <oznobkin@gmail.com>
2018-11-26 17:03:53 +0300

    refactor: breaking change in local game contract address

--- dapp/dapp.manifest.js
+++ dapp/dapp.manifest.js
@@ -2,7 +2,7 @@ const addressFunctions = {
   rinkeby: () => "0x868944cd75d4b70b6fb59254e998d5f757d7de0c",
   ropsten: () => "0x2d52802d5339EA8FBbDC21BA2ED651744dF8a6eA",
   mainnet: () => "",
-  local: () => "http://localhost:8545/contracts->Game"
+  local: () => "contracts->Game"
 }
 
 module.exports = {
